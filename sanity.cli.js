0bbbd5a5a45b46e3c995ca089d5d4511d16d0563
koladeore <koladeore@gmail.com>
2023-08-29 08:07:20 +0100

    first commit

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ic3s1i49',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,13 @@
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+    works, brands, testimonials, abouts, skills, workExperience, experiences, contact
+]
